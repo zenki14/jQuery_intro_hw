72c7dc83a469a0ef5f4c35fdf529f2e43f3b79bc
Dane Worsham <zenki14@gmail.com>
2015-01-20 15:55:43 -0500

    jQuery works as intended

--- main.js
+++ main.js
@@ -1,12 +1,23 @@
 
-$('.sectOne').on('click', function () {
-	$('.paraOne').toggleClass('pchanges')
-});
+// $('.sectOne').on('click', function () {
+// 	$('.paraOne').toggleClass('pchanges')
+// });
 
-$('.sectTwo').on('click', function () {
-	$('.paraTwo').toggleClass('pchanges')
-});
+// $('.sectTwo').on('click', function () {
+// 	$('.paraTwo').toggleClass('pchanges')
+// });
 
-$('.sectThree').on('click', function () {
-	$('.paraThree').toggleClass('pchanges')
+// $('.sectThree').on('click', function () {
+// 	$('.paraThree').toggleClass('pchanges')
+// });
+
+// $('.sect').on('click', function () {
+// $('.para').removeClass('pchanges');
+// 	$(this).find('para').addClass('pchanges')
+// });
+
+
+$('.sect').on('click', function () {
+  $('para').removeClass('pchanges');
+  $(this).next().addClass('pchanges');
 });
